23f7ab75a8dfe68ae28e53ae530030e048dbdcf6
Robbin Johansson <robbin@mediaempire.se>
2017-05-12 13:18:04 +0200

    Minor fix

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,10 +6,9 @@ const { mix } = require('laravel-mix');
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your theme. By default, we are compiling the Sass file for the theme 
+ | for your theme. By default, we are compiling the Sass file for the theme
  | as well as bundling up all the JS files.
  |
  */
 
-mix.setPublicPath('dist')
-   .js('src/main.js', 'scripts/');
+mix.js('src/main.js', '/');
