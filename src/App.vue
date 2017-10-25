5bdc47d70a38fec89f6a3a15b2044f3e4d6057d1
Robbin Johansson <robbinfellow@gmail.com>
2017-10-25 23:17:41 +0200

    Oops! :rofl:

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
-  <div id="app">
-    <vuebbble token="ac522ca5908d196031bba38306459d5cb341e7511c60ddd8dd668d2d4c1748f5" user="robbinfellow"></vuebbble>
-  </div>
+    <div id="app">
+        <vuebbble token="CLIENT_ACCESS_TOKEN" user="USERNAME"></vuebbble>
+    </div>
 </template>
 
 <script>
 import Vuebbble from './components/Vuebbble.vue'
 
 export default {
-  name: 'app',
-  components: {
-    Vuebbble
-  }
+    name: 'app',
+    components: {
+        Vuebbble
+    }
 }
 </script>
