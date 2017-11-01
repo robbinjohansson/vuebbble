d91965ee3b2f27194f7071fd76491f4416c89c96
Robbin Johansson <robbinfellow@gmail.com>
2017-11-01 17:15:49 +0100

    Revert "2.2.2"
    This reverts commit 5667e5a5a21fd381f903a68a18854986f0fc8d98.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import Vuebbble from './components/Vuebbble.vue';
 
-module.exports = {
-    install(Vue) {
-        Vue.component('vuebbble', Vuebbble);
-    },
-};
+function plugin (Vue) {
+    Vue.component('vuebbble', Vuebbble)
+}
+
+// Install by default if using the script tag
+if (typeof window !== 'undefined' && window.Vue) {
+    window.Vue.use(plugin)
+}
+
+export default plugin
+const version = '__VERSION__'
+// Export all components too
+export {
+    Vuebbble,
+    version
+}
