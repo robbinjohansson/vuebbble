141696b2b4772f943c913128203ee276949fa976
Robbin Johansson <robbinfellow@gmail.com>
2018-01-14 22:09:39 +0100

    Add automatic installation

--- src/index.js
+++ src/index.js
@@ -6,4 +6,9 @@ const plugin = {
   }
 }
 
+// Automatic installation if Vue has been added to the global scope.
+if (typeof window !== 'undefined' && window.Vue) {
+  window.Vue.use(plugin)
+}
+
 export default plugin
